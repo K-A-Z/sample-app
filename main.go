4085d5073048eeeea2fb10ac65ed8defca77ed31
Kazuya Nakane <nakane.kazuya@isid.co.jp>
2016-10-31 18:28:10 +0900

    add postgres

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+func getTodo(c *gin.Context) {
+	rows, err := db.Query("SELECT task from todo")
+	if err != nil {
+		c.String(http.StatusInternalServerError,
+			fmt.Sprintf("Error reading ticks: %q", err))
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var task string
+		if err := rows.Scan(&task); err != nil {
+			c.String(http.StatusInternalServerError, "Error :cant read task ::%q", err)
+			return
+		}
+
+		c.String(http.StatusOK, "Task: %s \n", task)
+	}
+}
+
 func main() {
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
